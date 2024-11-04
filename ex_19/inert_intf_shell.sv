4f56ac25dde5d0af315a6fe7d0f5ad47849c2715
abhinavnandwani <abhu.srt@gmail.com>
2024-11-04 12:03:48 -0600

    ex19

---
+++ ex_19/inert_intf_shell.sv
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////
+// Interfaces with ST 6-axis inertial sensor.  In  //
+// this application we only use Z-axis gyro for   //
+// heading of robot.  Fusion correction comes    //
+// from "gaurdrail" signals lftIR/rghtIR.       //
+/////////////////////////////////////////////////
+module inert_intf(clk,rst_n,strt_cal,cal_done,heading,rdy,lftIR,
+                  rghtIR,SS_n,SCLK,MOSI,MISO,INT,moving);
+
+  parameter FAST_SIM = 1;	// used to speed up simulation
+  
+  input clk, rst_n;
+  input MISO;					// SPI input from inertial sensor
+  input INT;					// goes high when measurement ready
+  input strt_cal;				// initiate claibration of yaw readings
+  input moving;					// Only integrate yaw when going
+  input lftIR,rghtIR;			// gaurdrail sensors
+  
+  output cal_done;				// pulses high for 1 clock when calibration done
+  output signed [11:0] heading;	// heading of robot.  000 = Orig dir 3FF = 90 CCW 7FF = 180 CCW
+  output rdy;					// goes high for 1 clock when new outputs ready (from inertial_integrator)
+  output SS_n,SCLK,MOSI;		// SPI outputs
+
+
+  //////////////////////////////////
+  // Declare any internal signal //
+  ////////////////////////////////
+  logic vld;		// vld yaw_rt provided to inertial_integrator
+  
+  
+  << Your code here >>
+
+
+
+
+
+
+
+
+
+  
+ 
+  ////////////////////////////////////////////////////////////////////
+  // Instantiate Angle Engine that takes in angular rate readings  //
+  // and acceleration info and produces a heading reading         //
+  /////////////////////////////////////////////////////////////////
+  inertial_integrator #(FAST_SIM) iINT(.clk(clk), .rst_n(rst_n), .strt_cal(strt_cal),.vld(vld),
+                           .rdy(rdy),.cal_done(cal_done), .yaw_rt(yaw_rt),.moving(moving),.lftIR(lftIR),
+                           .rghtIR(rghtIR),.heading(heading));
+						   
+
+endmodule
+	  
