68830a3ecbe0522bba890313dd4b12f2cb8d3394
Damion <119873269+Damion-14@users.noreply.github.com>
2024-12-06 13:33:56 -0600

    Update KnightsTour_tb.sv

--- tb/full_chip_functional/KnightsTour_tb.sv
+++ tb/full_chip_functional/KnightsTour_tb.sv
@@ -1,74 +1,69 @@
+
+import tb_tasks::*;
+
 module KnightsTour_tb();
 
- localparam FAST_SIM = 1;
- 
- 
- /////////////////////////////
- // Stimulus of type reg //
- /////////////////////////
- reg clk, RST_n;
- reg [15:0] cmd;
- reg send_cmd;
+    localparam FAST_SIM = 1;
+    
+    
+    /////////////////////////////
+    // Stimulus of type reg //
+    /////////////////////////
+    reg clk, RST_n;
+    reg [15:0] cmd;
+    reg send_cmd;
 
- ///////////////////////////////////
- // Declare any internal signals //
- /////////////////////////////////
- wire SS_n,SCLK,MOSI,MISO,INT;
- wire lftPWM1,lftPWM2,rghtPWM1,rghtPWM2;
- wire TX_RX, RX_TX;
- logic cmd_sent;
- logic resp_rdy;
- logic [7:0] resp;
- wire IR_en;
- wire lftIR_n,rghtIR_n,cntrIR_n;
- 
- //////////////////////
- // Instantiate DUT //
- ////////////////////
- KnightsTour iDUT(.clk(clk), .RST_n(RST_n), .SS_n(SS_n), .SCLK(SCLK),
-                  .MOSI(MOSI), .MISO(MISO), .INT(INT), .lftPWM1(lftPWM1),
-          .lftPWM2(lftPWM2), .rghtPWM1(rghtPWM1), .rghtPWM2(rghtPWM2),
-          .RX(TX_RX), .TX(RX_TX), .piezo(piezo), .piezo_n(piezo_n),
-          .IR_en(IR_en), .lftIR_n(lftIR_n), .rghtIR_n(rghtIR_n),
-          .cntrIR_n(cntrIR_n));
-         
- /////////////////////////////////////////////////////
- // Instantiate RemoteComm to send commands to DUT //
- ///////////////////////////////////////////////////
- RemoteComm iRMT(.clk(clk), .rst_n(RST_n), .RX(RX_TX), .TX(TX_RX), .cmd(cmd),
-            .snd_cmd(send_cmd), .cmd_snt(cmd_sent), .resp_rx_rdy(resp_rdy), .resp_rx_data(resp),.resp_clr_rx_rdy());
-          
- //////////////////////////////////////////////////////
- // Instantiate model of Knight Physics (and board) //
- ////////////////////////////////////////////////////
- KnightPhysics iPHYS(.clk(clk),.RST_n(RST_n),.SS_n(SS_n),.SCLK(SCLK),.MISO(MISO),
-                     .MOSI(MOSI),.INT(INT),.lftPWM1(lftPWM1),.lftPWM2(lftPWM2),
-           .rghtPWM1(rghtPWM1),.rghtPWM2(rghtPWM2),.IR_en(IR_en),
-           .lftIR_n(lftIR_n),.rghtIR_n(rghtIR_n),.cntrIR_n(cntrIR_n)); 
-          
-  ////////////////////////////////
-  //signals for task operations
-  //////////////////////////////
-  logic [11:0] prevErr, diffErr;
-  logic [16:0] prevOmega, diffOmega;
+    ///////////////////////////////////
+    // Declare any internal signals //
+    /////////////////////////////////
+    wire SS_n,SCLK,MOSI,MISO,INT;
+    wire lftPWM1,lftPWM2,rghtPWM1,rghtPWM2;
+    wire TX_RX, RX_TX;
+    logic cmd_sent;
+    logic resp_rdy;
+    logic [7:0] resp;
+    wire IR_en;
+    wire lftIR_n,rghtIR_n,cntrIR_n;
+    
+    //////////////////////
+    // Instantiate DUT //
+    ////////////////////
+    KnightsTour iDUT(.clk(clk), .RST_n(RST_n), .SS_n(SS_n), .SCLK(SCLK),
+                    .MOSI(MOSI), .MISO(MISO), .INT(INT), .lftPWM1(lftPWM1),
+                    .lftPWM2(lftPWM2), .rghtPWM1(rghtPWM1), .rghtPWM2(rghtPWM2),
+                    .RX(TX_RX), .TX(RX_TX), .piezo(piezo), .piezo_n(piezo_n),
+                    .IR_en(IR_en), .lftIR_n(lftIR_n), .rghtIR_n(rghtIR_n),
+                    .cntrIR_n(cntrIR_n));
+                    
+    /////////////////////////////////////////////////////
+    // Instantiate RemoteComm to send commands to DUT //
+    ///////////////////////////////////////////////////
+    RemoteComm iRMT(.clk(clk), .rst_n(RST_n), .RX(RX_TX), .TX(TX_RX), .cmd(cmd),
+                .snd_cmd(send_cmd), .cmd_snt(cmd_sent), .resp_rx_rdy(resp_rdy), .resp_rx_data(resp), .resp_clr_rx_rdy());
+                    
+    //////////////////////////////////////////////////////
+    // Instantiate model of Knight Physics (and board) //
+    ////////////////////////////////////////////////////
+    KnightPhysics iPHYS(.clk(clk),.RST_n(RST_n),.SS_n(SS_n),.SCLK(SCLK),.MISO(MISO),
+                        .MOSI(MOSI),.INT(INT),.lftPWM1(lftPWM1),.lftPWM2(lftPWM2),
+                        .rghtPWM1(rghtPWM1),.rghtPWM2(rghtPWM2),.IR_en(IR_en),
+                        .lftIR_n(lftIR_n),.rghtIR_n(rghtIR_n),.cntrIR_n(cntrIR_n)); 
 
-  task compareErr (input [11:0] prev, input [11:0] now, output [11:0] compE);
-    begin
-      compE = now - prev; 
-    end
-  endtask
+    logic [2:0] countCntr;
+    logic [1:0] countLft, countRght;
+    logic [14:0] x_pos, y_pos;          // Current positions of the knight
+    logic [14:0] x_expected, y_expected; // Expected positions of the knight
+    logic [7:0] move;
 
-  task compareOm(input [16:0] prev, input [16:0] now, output [16:0] compE);
-    begin
-      compE = now - prev; 
-    end
-  endtask
 
- always begin
+    always begin
         clk = 0;
         RST_n = 0;
         cmd = 16'h0000;
         send_cmd = 0;
+        countCntr = 0;
+        countLft = 0;
+        countRght = 0;
         
         @(negedge clk);
         RST_n = 1;
@@ -107,18 +102,43 @@ module KnightsTour_tb();
         @(posedge resp_rdy);
         @(negedge clk);
 
-        cmd = 16'h5BF1;
+        cmd = 16'h6220;
+
+	x_expected = 15'h2800;
+	y_expected = 15'h2800;
+
         send_cmd = 1;
 
         @(negedge clk);
-        send_cmd = 0;
+        send_cmd = 0; 
 
-        repeat (2) @(negedge iDUT.iCMD.cntrIR);
+        // check for proper handing off to tour cmd //
 
-        $display("Test passed for Latch Liberation front");
+        repeat (25) @(posedge iDUT.iTC.inc_mv_indx) begin
+            $display("INCREMENTING MOVE INDEX at time %0t", $time);
+		
+            x_pos = iPHYS.xx;
+            y_pos = iPHYS.yy;
+            move = iDUT.iTC.move;
+	    $display("Move: %b", move);	
+            casex (move) 
+                8'bxxxxxxx1: begin x_expected = x_expected + 15'd2 * 15'd4096; y_expected = y_expected + 15'd1 * 15'd4096; verifyCoordinates(x_expected, y_expected, x_pos, y_pos);    end // (2,1)
+                8'bxxxxxx10: begin x_expected = x_expected + 15'd1 * 15'd4096; y_expected = y_expected + 15'd2 * 15'd4096; verifyCoordinates(x_expected, y_expected, x_pos, y_pos);    end // (1,2)
+                8'bxxxxx100: begin x_expected = x_expected - 15'd1 * 15'd4096; y_expected = y_expected + 15'd2 * 15'd4096; verifyCoordinates(x_expected, y_expected, x_pos, y_pos);   end // (-2,1)
+                8'bxxxx1000: begin x_expected = x_expected - 15'd2 * 15'd4096; y_expected = y_expected + 15'd1 * 15'd4096; verifyCoordinates(x_expected, y_expected, x_pos, y_pos);   end // (-2,1)
+		8'bxxx10000: begin x_expected = x_expected - 15'd2 * 15'd4096; y_expected = y_expected - 15'd1 * 15'd4096; verifyCoordinates(x_expected, y_expected, x_pos, y_pos);  end // (-2,-1)
+                8'bxx100000: begin x_expected = x_expected - 15'd1 * 15'd4096; y_expected = y_expected - 15'd2 * 15'd4096; verifyCoordinates(x_expected, y_expected, x_pos, y_pos);  end // (-1,-2)
+                8'bx1000000: begin x_expected = x_expected + 15'd1 * 15'd4096; y_expected = y_expected - 15'd2 * 15'd4096; verifyCoordinates(x_expected, y_expected, x_pos, y_pos);   end // (1,-2)
+                8'b10000000: begin x_expected = x_expected + 15'd2 * 15'd4096; y_expected = y_expected - 15'd1 * 15'd4096; verifyCoordinates(x_expected, y_expected, x_pos, y_pos);   end // (2,-1)
+                default: $fatal("Unexpected move: %b", move);
+            endcase
+        end
+
+        $display("Tests done for latch liberation front");
         $stop();
     end
 
     always 
         #5 clk <= ~clk;
+
 endmodule
